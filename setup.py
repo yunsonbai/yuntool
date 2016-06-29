18f5552da0d9a22cce05b36fb3401e1bb0e05f95
yunson(baisong) <yunsonbai@sohu-inc.com>
2016-06-29 14:19:15 +0800

    dec 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="songdb",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     author="yunsonbai",
     author_email='1942893504@qq.com',
